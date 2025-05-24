7aeee493bd89420a56edeaf938431d051f82f35a
PetruVat <petruvatov@gmail.com>
2025-05-25 01:24:45 +0200

    HW_18

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from myapp.views import CategoryViewSet
 
 # Роутер для CategoryViewSet
@@ -13,6 +17,10 @@ urlpatterns = [
 
     # Подключение дополнительных маршрутов из приложения (если нужно)
     path('', include('myapp.urls')),
+
+    # JWT токены
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     # Роутер для категорий (включает CRUD и count_tasks)
     path('api/', include(router.urls)),
